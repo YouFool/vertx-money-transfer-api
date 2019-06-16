e5bab78aef02b7998064a4b4c10bbdab3e08c86b
LAPTOP-6GLD5642\João Heckmann <notbestkat@gmail.com>
2019-06-16 19:29:24 -0300

    Refactor ActionHelper

--- src/main/java/org/jlnh/ActionHelper.java
+++ src/main/java/org/jlnh/ActionHelper.java
@@ -11,75 +11,68 @@ import org.jlnh.model.Transaction;
 import java.util.NoSuchElementException;
 
 /**
- * Some helper code.
+ * Helper code to handle async results.
  */
 public class ActionHelper {
 
+    private static final String APPLICATION_JSON_CHARSET_UTF_8 = "application/json; charset=utf-8";
+    private static final String CONTENT_TYPE = "content-type";
+
+    private ActionHelper() {
+    }
+
     /**
-     * Returns a handler writing the received {@link AsyncResult} to the routing context and setting the HTTP status to
-     * the given status.
+     * Returns a handler writing the received {@link AsyncResult} to the routing context.
+     *
      * @param context the routing context
-     * @param status the status
      * @return the handler
      */
-    private static <T> Handler<AsyncResult<T>> writeJsonResponse(RoutingContext context, int status) {
+    static <T> Handler<AsyncResult<T>> ok(RoutingContext context) {
         return ar -> {
             if (ar.failed()) {
                 if (ar.cause() instanceof NoSuchElementException) {
-                    context.response().setStatusCode(404).end(ar.cause().getMessage());
+                    context.response() //
+                            .setStatusCode(404) //
+                            .end(ar.cause().getMessage());
                 } else {
                     context.fail(ar.cause());
                 }
             } else {
-                context.response().setStatusCode(status)
-                    .putHeader("content-type", "application/json; charset=utf-8")
-                    .end(Json.encodePrettily(ar.result()));
+                context.response().setStatusCode(200) //
+                        .putHeader(CONTENT_TYPE, APPLICATION_JSON_CHARSET_UTF_8) //
+                        .end(Json.encodePrettily(ar.result()));
             }
         };
     }
 
-    public static <T> Handler<AsyncResult<T>> ok(RoutingContext rc) {
-        return writeJsonResponse(rc,200);
-    }
-
-    public static <T> Handler<AsyncResult<T>> created(RoutingContext rc) {
-        return writeJsonResponse(rc,201);
-    }
-
-    public static Handler<AsyncResult<Void>> noContent(RoutingContext rc) {
-        return ar -> {
-            if (ar.failed()) {
-                if (ar.cause() instanceof NoSuchElementException) {
-                    rc.response().setStatusCode(404).end(ar.cause().getMessage());
-                } else {
-                    rc.fail(ar.cause());
-                }
-            } else {
-                rc.response().setStatusCode(204).end();
-            }
-        };
-    }
-
-    public static Handler<AsyncResult<Transaction>> handleTransfer(RoutingContext routingContext, Transaction transaction) {
+    /**
+     * Returns a handler to the transfer async result.
+     *
+     * @param context the routing context
+     * @param transaction the transaction
+     * @return the handler
+     */
+    static Handler<AsyncResult<Transaction>> handleTransfer(RoutingContext context, Transaction transaction) {
         return ar -> {
             if (ar.failed()) {
                 JsonObject failureJson = new JsonObject() //
                         .put("error", ar.cause().getMessage()); //
                 if (ar.cause() instanceof IllegalStateException) {
-                    failureJson.put("cause", "User does not have sufficient funds").put("code", 400);
-                    routingContext.response() //
+                    failureJson.put("cause", "User does not have sufficient funds")
+                            .put("code", 400);
+                    context.response() //
                             .setStatusCode(400) //
-                            .putHeader("content-type", "application/json; charset=utf-8") //
+                            .putHeader(CONTENT_TYPE, APPLICATION_JSON_CHARSET_UTF_8) //
                             .end(Json.encodePrettily(failureJson));
                 } else {
-                    routingContext.fail(ar.cause());
+                    context.fail(ar.cause());
                 }
             } else {
                 transaction.setTo(null); // Who transferred the money does not need to know his friend's balance
                 Json.prettyMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-                routingContext.response() //
+                context.response() //
                         .setStatusCode(201) //
-                        .putHeader("content-type", "application/json; charset=utf-8") //
+                        .putHeader(CONTENT_TYPE, APPLICATION_JSON_CHARSET_UTF_8) //
                         .end(Json.encodePrettily(transaction)); //TODO should encode ar.result()
             }
         };
